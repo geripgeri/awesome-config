3aae35a9074e87f5cabda43abc6c6d638a7f5cb1
Gergely Peidl <gergely@peidl.net>
2016-12-28 18:06:48 +0100

    Codebase reformatted.
    Closes #23

--- menugen/init.lua
+++ menugen/init.lua
@@ -5,13 +5,13 @@
 -- @release v3.5.5
 ---------------------------------------------------------------------------
 
-local menu_gen   = require("menubar.menu_gen")
+local menu_gen = require("menubar.menu_gen")
 local menu_utils = require("menubar.utils")
-local pairs      = pairs
-local ipairs     = ipairs
-local table      = table
-local string     = string
-local next       = next
+local pairs = pairs
+local ipairs = ipairs
+local table = table
+local string = string
+local next = next
 
 module("menugen")
 
@@ -24,37 +24,37 @@ menu_utils.wm_name = ""
 -- Use MenuBar Parsing Utils to build StartMenu for Awesome
 -- @return awful.menu compliant menu items tree
 function build_menu()
-	local result = {}
-	local menulist = menu_gen.generate()
+    local result = {}
+    local menulist = menu_gen.generate()
 
-	for k,v in pairs(menu_gen.all_categories) do
-		table.insert(result, {k, {}, v["icon"] } )
-	end
+    for k, v in pairs(menu_gen.all_categories) do
+        table.insert(result, { k, {}, v["icon"] })
+    end
 
-	for k, v in ipairs(menulist) do
-		for _, cat in ipairs(result) do
-			if cat[1] == v["category"] then
-				table.insert( cat[2] , { v["name"], v["cmdline"], v["icon"] } )
-				break
-			end
-		end
-	end
+    for k, v in ipairs(menulist) do
+        for _, cat in ipairs(result) do
+            if cat[1] == v["category"] then
+                table.insert(cat[2], { v["name"], v["cmdline"], v["icon"] })
+                break
+            end
+        end
+    end
 
-	-- Cleanup Things a Bit
-	for k,v in ipairs(result) do
-		-- Remove Unused Categories
-		if not next(v[2]) then
-			table.remove(result, k)
-		else
-			--Sort entries Alphabetically (by Name)
-			table.sort(v[2], function (a,b) return string.byte(a[1]) < string.byte(b[1]) end)
-			-- Replace Catagory Name with nice name
-			v[1] = menu_gen.all_categories[v[1]].name
-		end
-	end
+    -- Cleanup Things a Bit
+    for k, v in ipairs(result) do
+        -- Remove Unused Categories
+        if not next(v[2]) then
+            table.remove(result, k)
+        else
+            --Sort entries Alphabetically (by Name)
+            table.sort(v[2], function(a, b) return string.byte(a[1]) < string.byte(b[1]) end)
+            -- Replace Catagory Name with nice name
+            v[1] = menu_gen.all_categories[v[1]].name
+        end
+    end
 
-	-- Sort Categories Alphabetically Also
-	table.sort(result, function(a,b) return string.byte(a[1]) < string.byte(b[1]) end)
+    -- Sort Categories Alphabetically Also
+    table.sort(result, function(a, b) return string.byte(a[1]) < string.byte(b[1]) end)
 
-	return result
+    return result
 end

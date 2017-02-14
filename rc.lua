f48b7eb7d0f7064c4677fdeea64f35d883b8ee82
Gergely Peidl <gergely@peidl.net>
2017-02-14 09:42:31 +0100

    Unused function removed.

--- rc.lua
+++ rc.lua
@@ -22,14 +22,6 @@ local menubar = require("menubar")
 -- }}}
 
 -- {{{ Error handling
-if awesome.startup_errors then
-    naughty.notify({
-        preset = naughty.config.presets.critical,
-        title = "Oops, there were errors during startup!",
-        text = awesome.startup_errors
-    })
-end
-
 do
     local in_error = false
     awesome.connect_signal("debug::error", function(err)
